0b12eb247384bb8d824ccebd1b9f9d637a176394
anizmo <potdar.anuj@gmail.com>
2023-04-21 12:55:01 -0400

    Created Ecommerce Angular frontend from the Baeldung guide - Added angular frontend code from the tutorial

--- app/src/main/java/com/arm/ecommerce/service/ProductServiceImpl.java
+++ app/src/main/java/com/arm/ecommerce/service/ProductServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
